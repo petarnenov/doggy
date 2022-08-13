4cacb355bab41245a13f0d6e8d2d0b8d46a07e3a
Petar Nenov <petarnenovpetrov@gmail.com>
2022-08-13 18:50:10 +0300

    play: table

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className='title'>
-      Pokemon Search
+    <div style={{
+      margin: "auto",
+      width: 800,
+      paddingTop: "1rem"
+    }}>
+      <h1 className="title">
+        Pokemon Search
+      </h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Bulbasaur</td>
+            <td>Grass, Poison</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
